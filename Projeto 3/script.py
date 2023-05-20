af093ba09267837dd567a9396a4244658a44c7b6
gZanda <gabrielfzanda@gmail.com>
2023-05-19 22:05:36 -0300

    Continue

--- Projeto 3/script.py
+++ Projeto 3/script.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
-navegador = webdriver.Firefox()
+import pandas
 
 # 1° Abrir o navegador
+navegador = webdriver.Firefox()
+navegador.get("https://www.google.com.br/?hl=pt-BR")
 
 # 2° Importar a base de dados
+tabela = pandas.read_excel("commodities.xlsx")
+print(tabela)
 
-# 3° Pegar o preço atual de cada produto
+# 3° Pegar o preço atual de cada produto ( algum site )
+navegador.get("https://www.melhorcambio.com/milho-hoje")    # Abre o site
+navegador.find_element("xpath",'//*[@id="comercial"]')      # Pega o campo da cotação
+
+# O que podemos fazer com esse campo ?
+# .send_keys() - Escrever
+# .click()
+# .get_attribute() - Pegar os dados
 
 # 4° Atualizar o Preço na base de dados
 
