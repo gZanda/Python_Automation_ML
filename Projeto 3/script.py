24312480c2380ffd033e7768d468d3579308dfcd
gZanda <gabrielfzanda@gmail.com>
2023-05-19 21:45:03 -0300

    Selenium

---
+++ Projeto 3/script.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+navegador = webdriver.Firefox()
+
+# 1° Abrir o navegador
+
+# 2° Importar a base de dados
+
+# 3° Pegar o preço atual de cada produto
+
+# 4° Atualizar o Preço na base de dados
+
+# 5° Decidir comprar ou não
+
+# 6° Exportar a base Atualizada
